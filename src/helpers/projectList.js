6363c7a1f63eb45d94a86ad827403d84cddf57b4
Galiya <galiyaga@yandex.ru>
2025-08-14 18:09:30 +0300

    добавление четвертого проекта в массив

--- src/helpers/projectList.js
+++ src/helpers/projectList.js
@@ -4,12 +4,14 @@ import project02 from "../img/projects/02.jpg";
 import project02Big from "../img/projects/02-big.jpg";
 import project03 from "../img/projects/03.jpg";
 import project03Big from "../img/projects/03-big.gif";
+import project04 from "../img/projects/04.jpg";
+import project04Big from "../img/projects/04-big.jpg";
 import imgGH from "./gitHub-black.svg";
 import imgVC from "./vercel-black.svg";
 
 const projects = [
   {
-    title: "Сайт по поиску публикаций",
+    title: "Сайт по поиску публикаций о компании по ИНН",
     skills: "Node.js, React, Redux, HTML5, CSS3",
     img: project01,
     imgBig: project01Big,
@@ -37,26 +39,48 @@ const projects = [
       gitHubLink: {
         title: "GitHub repo",
         link: "https://github.com/Galiyaga/task-manager-ts.project",
+        icon: imgGH,
       },
       vercelLink: {
         title: "On the domain",
         link: "https://task-manager-ts-project.vercel.app/login",
+        icon: imgVC,
       },
     },
   },
   {
     title: "Таск-менеджер на ванильном JS",
-    skills: "Node.js, VanillaJS, HTML5, CSS3",
+    skills: "Node.js, VanillaJS, Dragula, HTML5, CSS3",
     img: project03,
     imgBig: project03Big,
     links: {
       gitHubLink: {
         title: "GitHub repo",
         link: "https://github.com/Galiyaga/task-manager",
+        icon: imgGH,
       },
       vercelLink: {
         title: "On the domain",
         link: "task-manager-vanillajs.vercel.app",
+        icon: imgVC,
+      },
+    },
+  },
+  {
+    title: "Верстка лендинга дизайнерского сайта",
+    skills: "Node.js, VanillaJS, Dragula, HTML5, CSS3",
+    img: project04,
+    imgBig: project04Big,
+    links: {
+      gitHubLink: {
+        title: "GitHub repo",
+        link: "https://github.com/Galiyaga/lending_module18/tree/master",
+        icon: imgGH,
+      },
+      vercelLink: {
+        title: "On the domain",
+        link: "lending-module18.vercel.app",
+        icon: imgVC,
       },
     },
   },
