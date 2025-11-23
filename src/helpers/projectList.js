ad3455dfb3a507a20ea84718f0d02164c5f8e5ff
Galiya <galiyaga@yandex.ru>
2025-11-23 11:38:11 +0300

    заменена картинка 3го проекта на странице проектов

--- src/helpers/projectList.js
+++ src/helpers/projectList.js
@@ -2,7 +2,7 @@ import project01 from "../img/projects/01.jpg";
 import project01Big from "../img/projects/01-big.jpg";
 import project02 from "../img/projects/02.jpg";
 import project02Big from "../img/projects/02-big.jpg";
-import project03 from "../img/projects/03.png";
+import project03 from "../img/projects/03.jpg";
 import project03Big from "../img/projects/03-gif.gif";
 import project04 from "../img/projects/04.jpg";
 import project04Big from "../img/projects/04-big.jpg";
