708a07e437e8b6390b6ed3ad8ffc1419d57df393
Galiya <galiyaga@yandex.ru>
2025-11-25 13:57:13 +0300

    кнопка загрузки портфолио доступна, анимированная картинка центрирована под title

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -11,10 +11,14 @@ const Header = () => {
           loading="lazy"
         />
       </div>
-      <div class="header-bg">
-        <img className="header-bg__img" src="/foto/header-bg.png" alt="Элемент дизайна" />
-      </div>
       <div className="header__wrapper">
+        <div class="header-bg">
+          <img
+            className="header-bg__img"
+            src="/foto/header-bg.png"
+            alt="Элемент дизайна"
+          />
+        </div>
         <h1 className="header__title">
           <strong>
             Hi, my name is <em>Galiya</em>
@@ -26,7 +30,10 @@ const Header = () => {
             who's always excited to learn new things and bring ideas to life.
           </p>
         </div>
-        <a href="https://kazan.hh.ru/resume_converter/%D0%AE%D0%BD%D1%83%D1%81%D0%BE%D0%B2%D0%B0%20%D0%93%D0%B0%D0%BB%D0%B8%D1%8F%20%D0%9A%D1%83%D1%82%D0%B4%D1%83%D1%81%D0%BE%D0%B2%D0%BD%D0%B0.pdf?hash=8ff229efff0f8d00610039ed1f644f43487052&type=pdf&hhtmFrom=resume&hhtmSource=resume" className="btn">
+        <a
+          href="https://kazan.hh.ru/resume_converter/%D0%AE%D0%BD%D1%83%D1%81%D0%BE%D0%B2%D0%B0%20%D0%93%D0%B0%D0%BB%D0%B8%D1%8F%20%D0%9A%D1%83%D1%82%D0%B4%D1%83%D1%81%D0%BE%D0%B2%D0%BD%D0%B0.pdf?hash=8ff229efff0f8d00610039ed1f644f43487052&type=pdf&hhtmFrom=resume&hhtmSource=resume"
+          className="btn"
+        >
           Download CV
         </a>
       </div>
